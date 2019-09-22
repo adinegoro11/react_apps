261aa72861e7d1ab259285c369c0a738d377e6d3
adinegoro11 <adinegoro11@gmail.com>
2019-09-22 12:08:59 +0700

    Passing and receiving props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
+// import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author ="Cristiano"/>
+            <CommentDetail author ="Sergio Ramos" />
+            <CommentDetail author ="Kevin de Bryune" />
+            <CommentDetail author ="David de Gea" />
+            <CommentDetail author ="Gianluigi Buffon" />
         </div>
     );
 };
