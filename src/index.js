74f390806c2b46d6d669e108b90801e4509e6206
adinegoro11 <adinegoro11@gmail.com>
2019-09-29 05:01:54 +0700

    Conditionally rendering content

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component{
-    constructor(props){
+class App extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {lat: 666666, errorMessage:''};
+        this.state = { lat: 666666, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
-                this.setState({lat: position.coords.latitude});
+                this.setState({ lat: position.coords.latitude });
             },
             err => {
-                this.setState({errorMessage: err.message});
+                this.setState({ errorMessage: err.message });
             }
         );
     }
 
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br />
-                Error: {this.state.errorMessage}
-            </div>
-        );
+        if (this.state.lat && !this.state.errorMessage) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        return <div>Loading...</div>;
     }
 };
 
