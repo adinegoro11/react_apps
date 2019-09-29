b8cc19f514299e5f4b610a963b935b1d50622157
adinegoro11 <adinegoro11@gmail.com>
2019-09-29 21:59:14 +0700

    Viewing request results

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID 4e32efa4909930498766c4998aee5035bedece007413bed3fa267cbf8d8e6e60'
+            }
+
+        });
     }
 
     render() {
